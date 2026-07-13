320c54f4ea51f24ccdd1d1e87cd70e22d3548150
sepehr-davarnia <sepehr.davarnia@gmail.com>
2026-07-13 04:08:23 +0330

    Senior SEO UI UX full-stack review: Atelier.Web/Areas/Admin/ViewModels/LoginViewModel.cs

--- Atelier.Web/Areas/Admin/ViewModels/LoginViewModel.cs
+++ Atelier.Web/Areas/Admin/ViewModels/LoginViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace Atelier.Web.Areas.Admin.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Atelier.Web.Areas.Admin.ViewModels;
 
 public class LoginViewModel
 {
+    [Required(ErrorMessage = "نام کاربری الزامی است.")]
+    [StringLength(256)]
     public string? UserName { get; set; }
+
+    [Required(ErrorMessage = "رمز عبور الزامی است.")]
+    [StringLength(256)]
     public string? Password { get; set; }
     public bool RememberMe { get; set; }
 }
